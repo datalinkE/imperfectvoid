902c964eb1fcff99f2dc747cc04769ce34b16645
A. Pichugin <01.datalink@gmail.com>
2014-04-23 23:25:20 +0400

    convinient logger class

--- jni/main.cpp
+++ jni/main.cpp
@@ -1,26 +1,33 @@
 #include <jni.h>
 #include <android_native_app_glue.h>
-#include <android/log.h>
+#include <util/logger.hpp>
 
 void android_main(android_app* pState)
 {
 	app_dummy();
-	__android_log_print(ANDROID_LOG_DEBUG, "IMP", "Hello world\n");
+
+	DLOG() << "hello";
+	DLOG() << "world";
+	DLOG() << "hello" << "world";
 
 	int events;
-	struct android_poll_source* pSource;
-	int ident = ALooper_pollAll(0, 0, &events, (void**)&pSource);
-	if (ident >= 0)
-	{
-		if (pSource)
-		{
-			pSource->process(pState, pSource);
-		}
+	android_poll_source* pSource;
 
-		if (pState->destroyRequested)
+	while(true)
+	{
+		int ident = ALooper_pollAll(0, 0, &events, (void**)&pSource);
+		if (ident >= 0)
 		{
-			__android_log_print(ANDROID_LOG_DEBUG, "IMP", "Destroy\n");
+			if (pSource)
+			{
+				pSource->process(pState, pSource);
+			}
+
+			if (pState->destroyRequested)
+			{
+				DLOG() << "destroy";
+				break;
+			}
 		}
 	}
-
 }
---
+++ jni/util/logger.hpp
@@ -0,0 +1,46 @@
+/*
+ * Logger.hpp
+ *
+ *  Created on: 22.04.2014
+ *      Author: Enter
+ */
+
+#ifndef LOGGER_HPP_
+#define LOGGER_HPP_
+
+#include <android/log.h>
+#include <sstream>
+#include <string>
+
+namespace Util
+{
+	class Logger
+	{
+	public:
+		Logger(int priority, const char* tag)
+			:	m_priority(priority)
+			,	m_tag(tag)
+		{};
+
+		template<class T>
+		inline Logger& operator << (const T& value)
+		{
+			m_content << value;
+			return *this;
+		}
+
+		~Logger()
+		{
+			__android_log_write(m_priority, m_tag.c_str(), m_content.str().c_str() );
+		}
+
+	private:
+		int m_priority;
+		std::string m_tag;
+		std::stringstream m_content;
+	};
+}
+
+#define DLOG() Util::Logger(ANDROID_LOG_DEBUG, __func__)
+
+#endif /* LOGGER_HPP_ */
